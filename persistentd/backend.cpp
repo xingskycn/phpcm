02ce7a5439bd4ddb15531425610a908ab8ad4000
lynx <lynx@phy-lynx.woman.ru>
2014-11-06 17:19:07 +0300

    [*] oc

--- persistentd/backend.cpp
+++ persistentd/backend.cpp
@@ -5,12 +5,16 @@
 
 Backend::Backend()
 {
-
+    reactor = new Reactor();
+    reactor->addHandler(Event_On_Set, this->);
+    reactor->addHandler(Event_On_Del);
+    reactor->addHandler(Event_On_Get);
+    reactor->addHandler(Event_On_Exec);
 }
 
 void Backend::setCallback(BackendResponderNamed)
 {
-
+    this->callback = callback;
 }
 
 void* Backend::start()
@@ -19,7 +23,8 @@ void* Backend::start()
     INFO("backend: starting: %s", "ok");
 }
 
-void addEvent(ReactorEStruct event)
+void Backend::addEvent(ReactorEStruct event)
 {
-
+    reactor->addEventAsync(event, this->callback);
 }
+
--- persistentd/backend.hpp
+++ persistentd/backend.hpp
@@ -7,14 +7,25 @@
 #define BackendResponder ReactorUserCallback
 #define BackendResponderNamed ReactorUserCallbackNamed
 
+#define Event_On_Set 10
+#define Event_On_Get 11
+#define Event_On_Del 12
+#define Event_On_Exec 100
+
 class Backend : public IServerThread {
     public:
         Backend();
         void setCallback(BackendResponder);
         virtual void* start(); //start server
         void addEvent(ReactorEStruct event);
+
+        ReactorRVal handler_On_set(void *data, size_t length, Reactor* reactor);
+        ReactorRVal handler_On_Get(void *data, size_t length, Reactor* reactor);
+        ReactorRVal handler_On_Del(void *data, size_t length, Reactor* reactor);
+        ReactorRVal handler_On_Exec(void *data, size_t length, Reactor* reactor);
     private:
         Reactor *reactor;
         std::queue<ReactorEStruct> inQueue;
+
         BackendResponderNamed;
 };
